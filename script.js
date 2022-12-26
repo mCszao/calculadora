49aefd966120fd11d7b2dd101e1ff90179a68510
kuohakuu <110683088+mCszao@users.noreply.github.com>
2022-12-26 10:09:58 -0400

    Implementação da logica principal da calculadora

--- script.js
+++ script.js
@@ -3,23 +3,36 @@ let historicCount = [];
 buttons.forEach((button) => {
   button.addEventListener("click", mainFuc);
 });
+let operator;
 
 function mainFuc() {
   let button = this.innerText;
   let result = document.querySelector(".resultado");
-  if (+button) result.value += button;
+  console.log(historicCount);
+  // console.log(operator);
+  if (+button || button == "0") result.value += button;
   if (!+button) {
     if (button == "C") result.value = "";
     if (
       button == "+" ||
       button == "-" ||
       button == "/" ||
-      button == "x" ||
+      button == "*" ||
       button == "."
     ) {
-      result.value += button;
+      if (operator === undefined) {
+        result.value += button;
+        operator = button;
+      } else {
+        result.value = eval(result.value);
+        historicCount.push(result.value);
+        result.value += button;
+        operator = button;
+      }
     }
     if (button == "=") {
+      result.value = eval(result.value);
+      historicCount.push(result.value);
     }
   }
 }
